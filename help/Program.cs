96b928b4d709731951469198d54d2aaa34fa79a6
Bessenyei Krisztián <bessenyei.krisztian@vereb.dc>
2019-09-20 16:39:16 +0200

    Add project files.

---
+++ help/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace help
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia hogy hívnak");
+            var n = Console.ReadLine();
+            Console.WriteLine("milyen szép a szemed {n}");
+            Console.WriteLine("szeretel phoghamozni");
+            var v = Console.ReadLine();
+            if (v.ToLower() == "igen" )
+            {
+                Console.WriteLine("az fasza");
+            }
+            else
+            {
+                Console.WriteLine("nem vagyunk barátok");
+            }
+            Console.ReadKey();
+        }
+    }
+}
