9d939a2d06eeb55b806cf94955d1432ae1b73752
bessenyei krisztián <besenyeikrisztian01@outlook.com>
2019-09-20 17:27:30 +0200

    csillag

--- help/Program.cs
+++ help/Program.cs
@@ -8,6 +8,7 @@ namespace help
 {
     class Program
     {
+       static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine("Szia hogy hívnak");
@@ -22,6 +23,13 @@ namespace help
             else
             {
                 Console.WriteLine("nem vagyunk barátok");
+            }
+            Console.WriteLine("nesze itt van 100 csillag");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.SetCursorPosition(rnd.Next(Console.WindowWidth),rnd.Next(Console.WindowHeight));
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 14);
+                Console.Write("*");
             }
             Console.ReadKey();
         }
